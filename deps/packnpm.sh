3c439e1c8c5e2b0dcffefd9132280dd0d4567d12
Enrico Giordani <enrico.giordani@gmail.com>
2016-08-23 15:20:51 +0200

    v0.3.1.4

--- deps/packnpm.sh
+++ deps/packnpm.sh
@@ -10,7 +10,7 @@ cd ../build_scripts/replace_urls/
 python replace_urls.py deps-npm-lib-_jx.js.template
 cd $cwd
 
-VER=311b
+VER=314
 tar -zcvf npmjx$VER.tar.gz npm
 cp npmjx$VER.tar.gz ~/.jx/
 cd ~/.jx/
