f098b24c26a43dac27785364d7b6b25d631541cf
ZepHakase <dmarinom@gmail.com>
2016-11-07 10:29:48 +0100

    Remove SIGSEGV signal handler.
    This is a rollback of https://github.com/janeasystems/jxcore-private/commit/ac440506b45e6d59178d9eb1c73663566e57be93
    Catching the SIGSEGV in jxcore is not the correct way to handle the
    problem. Moreover, we were calling exit(1) that is not acceptable when
    jxcore is embedded.

--- test/simple/test-regress-GH-4015.js
+++ test/simple/test-regress-GH-4015.js
@@ -18,10 +18,5 @@ exec(cmd, function(err, stdout, stderr) {
   if(process.versions.v8)
     assert(/RangeError: Maximum call stack size exceeded/.test(stderr));
   else
-    // On recent Ubuntu releases the test was failing because the 
-    // err.signal value doesn't get set properly.
-    // The current fix in JXcore catches the SIGSEGV signal at a place
-    // where is too late to get the err object filled properly, therefore
-    // in the test we need to use the error code value (128 + 11).
-    assert(err.code == 139 || err.signal == "SIGSEGV" );
+    assert(/Segmentation fault/.test(stderr) || err.signal==='SIGSEGV')
 });
