652dae63f899d3ffe79eef1957f6cb9a05f11172
Alexis Campailla <alexis@janeasystems.com>
2017-01-19 01:31:15 +0000

    iOS test app

---
+++ tools/ios-test/iOS-Test/iOS-Test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  Licensed under MIT
+//  Copyright © 2017 Alexis Campailla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tools/ios-test/iOS-Test/iOS-Test/jxcore_callback.hpp
@@ -0,0 +1,14 @@
+//
+//  jxcore_callback.hpp
+//  JXcoreHTTPServer
+//
+//  Created by enrico on 11/22/16.
+//  Copyright © 2016 Nubisa. All rights reserved.
+//
+
+#ifndef jxcore_callback_hpp
+#define jxcore_callback_hpp
+
+#include <stdio.h>
+
+#endif /* jxcore_callback_hpp */
