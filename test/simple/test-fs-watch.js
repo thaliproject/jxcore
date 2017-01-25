cbe6726e2f6237070f6f51456fd5ddaa71db0624
Alexis Campailla <alexis@janeasystems.com>
2017-01-25 17:13:45 +0100

    iOS: temporarily disabling failing tests
    These tests always fail for unknown reasons on iOS.
    
    Disabling temporarily so that they don't affect other tests in test runs.

--- test/simple/test-fs-watch.js
+++ test/simple/test-fs-watch.js
@@ -1,5 +1,10 @@
 // Copyright & License details are available under JXCORE_LICENSE file
 
+if (process.platform === 'ios') {
+  console.error('Skipping: the test does not work on iOS');
+  process.exit(0);
+}
+
 var common = require('../common');
 var assert = require('assert');
 var path = require('path');
